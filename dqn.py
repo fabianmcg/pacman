67aa676f0a2dfd6b547aae4a3fb38092185adb87
fmorac <fabian.mcg@crpl.udel.edu>
2021-06-03 13:36:25 -0400

    Fixing DQN

--- dqn.py
+++ dqn.py
@@ -10,17 +10,18 @@ import tensorflow as tf
 
 from pacman import GameState
 
-# def network(hiddenLayers, unitsLast, stateShape, init, optimizer, loss):
-#     from tensorflow.keras.models import Model
-#     from tensorflow.keras.layers import Concatenate, Dense, Input, Flatten
-#     modelInput = Input(shape=stateShape, name="Input")
-#     layer = Flatten(name="Flatten")(modelInput)
-#     for i, k in enumerate(hiddenLayers):
-#         layer = Dense(k, activation='relu', kernel_initializer=init, name='H' + str(i))(layer)
-#     modelOutput = Concatenate(name="Output")([Dense(1, name=str(k))(layer) for k in range(unitsLast)])
-#     model = Model(inputs=modelInput, outputs=modelOutput)
-#     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
-#     return model
+def convolutionalNetwork(convLayers, denseLayers, stateShape, optimizer, init = None, loss = tf.losses.MeanSquaredError()):
+    from tensorflow.keras.layers import Dense, Flatten, Conv2D
+    model = tf.keras.Sequential()
+    model.add(Conv2D(convLayers[0][0], convLayers[0][1], activation='relu', kernel_initializer=init, input_shape=stateShape))
+    for x in convLayers[1:]:
+        model.add(Conv2D(x[0], x[1], activation='relu', kernel_initializer=init))
+    model.add(Flatten())
+    for x in denseLayers[0:-1]:
+        model.add(Dense(x, activation='relu', kernel_initializer=init))
+    model.add(tf.keras.layers.Dense(denseLayers[-1], activation='linear', kernel_initializer=init))
+    model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+    return model
 
 def network(layersUnits, stateShape, optimizer, init = None, loss = tf.losses.MeanSquaredError()):
     from tensorflow.keras.layers import Dense, Flatten
@@ -32,8 +33,21 @@ def network(layersUnits, stateShape, optimizer, init = None, loss = tf.losses.Me
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
     return model
 
+
+class DQNState:
+    def __init__(self, state, action, reward):
+        self.state = state
+        self.action = action
+        self.reward = reward
+        self.outcomeState = None
+        self.outcomeActions = None
+        self.isOutcomeTerminal = None
+    
+    def setOutcome(self, state):
+        pass
+
 class DQNAgent(Agent):
-    def __init__(self, alpha = 0.25, gamma = 0.75, experienceSize = 400, C = 3, numTraining = 0, epsilon = 0.1, learningRate = 0.1, minibatchSize = 100, **kwargs):
+    def __init__(self, alpha = 0.25, gamma = 0.75, experienceSize = 400, minibatchSize = 100, C = 10, numTraining = 0, epsilon = 1., learningRate = 0.1, **kwargs):
         self.it = 0
         self.git = 0
         self.index = 0
@@ -45,23 +59,26 @@ class DQNAgent(Agent):
         self.experienceReplay = [None] * int(experienceSize)
         self.C = int(C)
         self.epsilon = float(epsilon)
-        self.epsilonArray = [float(self.epsilon), 1 - float(self.epsilon)]
         self.learningRate = float(learningRate)
         self.minibatchSize = int(minibatchSize)
         self.rewards = Rewards(**kwargs)
         self.QNetwork = None
         self.QQNetwork = None
         self.random = np.random.default_rng(int(kwargs["seed"])) if "seed" in kwargs else np.random.default_rng(12345)
-        self.architecture = (8, 4, 5)
+        self.architecture = (10, 5, 5)
         self.previousState = None
     
+    def epsilonArray(self):
+        return [float(self.epsilon), 1 - float(self.epsilon)]
+
     def initNetworks(self, stateShape):
         tf.device("/gpu:0")
         from tensorflow.keras.initializers import RandomUniform, zeros
-        self.QNetwork = network(self.architecture, stateShape, tf.keras.optimizers.Adam(learning_rate=self.learningRate), RandomUniform(minval=-0.05, maxval=0.05, seed=None))
-        self.QQNetwork = network(self.architecture, stateShape, tf.keras.optimizers.Adam(learning_rate=self.learningRate), zeros())
+        self.QNetwork = convolutionalNetwork([(1, 2)], self.architecture, stateShape, tf.keras.optimizers.Adam(learning_rate=self.learningRate), RandomUniform(minval=-0.05, maxval=0.05, seed=None))
+        self.QQNetwork = tf.keras.models.clone_model(self.QNetwork)
 
     def registerInitialState(self, gameState):
+        self.rewards.initial(gameState)
         if self.it == 0:
             state = gameStateTensor(gameState)
             self.initNetworks(state.shape)
@@ -94,6 +111,7 @@ class DQNAgent(Agent):
         y = self.gamma * QQ
         y[isTerminal > 0] = 0
         y += rewards
+        print(y)
         Q = self.QNetwork(miniBatchQ).numpy()
         Q[tuple(range(actions.size)), tuple(actions)] = y
         self.QNetwork.fit(miniBatchQ, Q, verbose=0)
@@ -105,7 +123,7 @@ class DQNAgent(Agent):
         actions, actionsIndexes = getActions(gameState)
         if self.previousState != None:
             self.learn(state, gameState, actionsIndexes)
-        randomAction = self.random.choice([False, True], p=self.epsilonArray)
+        randomAction = self.random.choice([False, True], p=self.epsilonArray())
         if randomAction:
             action = self.random.integers(0, len(actions))
         else:
