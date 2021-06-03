e82e76d68eeb9e1ebbd25183f90a2b1b888bd2ba
Fabian Mora <fmora.dev@gmail.com>
2021-06-03 15:32:49 -0400

    DQN for try

--- agentUtil.py
+++ agentUtil.py
@@ -17,7 +17,7 @@ DIR2CODE = {
 def getActions(gameState):
     actions = gameState.getLegalActions()
     return actions, [DIR2CODE[a] for a in actions]
-    
+
 def sumSumToN(n):
     return int((n * (n + 1) * (n + 2)) / 6)
 
@@ -113,21 +113,21 @@ class Rewards:
         self.capsules = len(gameState.getCapsules())
         self.score = 0
 
-    def reward(self, gameState):
-        pacmanPosition = gameState.getPacmanPosition()
-        food = gameState.getNumFood()
-        capsules = len(gameState.getCapsules())
-        nearestGhost = min([manhattanDistance(pacmanPosition, ghost.getPosition()) for ghost in gameState.getGhostStates()])
-        score = 500. * gameState.isWin() - 500. * gameState.isLose() + 20 * (self.food != food) - (nearestGhost < 3) * 5 - 1 + 100 * (self.capsules != capsules)
-        self.score = score
-        self.food = food
-        self.capsules = capsules
-        return score / 500.
-
     # def reward(self, gameState):
-    #     score = gameState.getScore() - self.score
-    #     self.score = gameState.getScore()
+    #     pacmanPosition = gameState.getPacmanPosition()
+    #     food = gameState.getNumFood()
+    #     capsules = len(gameState.getCapsules())
+    #     nearestGhost = min([manhattanDistance(pacmanPosition, ghost.getPosition()) for ghost in gameState.getGhostStates()])
+    #     score = 500. * gameState.isWin() - 500. * gameState.isLose() + 20 * (self.food != food) - (nearestGhost < 3) * 5 - 1 + 100 * (self.capsules != capsules)
+    #     self.score = score
+    #     self.food = food
+    #     self.capsules = capsules
     #     return score / 500.
+
+    def reward(self, gameState):
+        score = gameState.getScore() - self.score
+        self.score = gameState.getScore()
+        return score / 501.
         
     def reset(self):
         self.score = 0
