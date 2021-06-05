0beea3bf01f0ca08e3f00c1e3bdab5d3ef3938cf
fmorac <fabian.mcg@crpl.udel.edu>
2021-06-04 22:00:02 -0400

    DQN for try

--- wphc.py
+++ wphc.py
@@ -6,8 +6,9 @@ from game import Agent
 from agentUtil import *
 from phc import PHCAgent
 
+
 class WPHCAgent(PHCAgent):
-    def __init__(self, delta = 0.5, deltaLose = 1, **kwargs):
+    def __init__(self, delta=0.5, deltaLose=1, **kwargs):
         super().__init__(delta=delta, **kwargs)
         self.deltaLose = deltaLose
         self.C = dict()
@@ -18,7 +19,7 @@ class WPHCAgent(PHCAgent):
             self.Q[state] = np.full(numActions, 0.0)
             self.Pi[state] = np.full(numActions, 1.0 / numActions)
             self.C[state] = 0
-            self.PiAvg[state] = np.full(numActions, 1. / numActions)
+            self.PiAvg[state] = np.full(numActions, 1.0 / numActions)
 
     def learn(self, state, gameState):
         previousState = self.previousState.state
