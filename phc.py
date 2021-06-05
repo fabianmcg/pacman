e09950354ee6f6b002249525edce80efc3afcbcc
fmorac <fabian.mcg@crpl.udel.edu>
2021-06-04 20:23:45 -0400

    PHC & WPHC working again

--- phc.py
+++ phc.py
@@ -2,61 +2,51 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from game import Agent
+from PacmanAgent import PacmanAgent, PacmanState
 from agentUtil import *
 
-class PHCAgent(Agent):
-    def __init__(self, alpha = 0.25, gamma = 0.75, delta = 0.75, numTraining = 0, **kwargs):
-        self.it = 0
-        self.index = 0
+
+class PHCAgent(PacmanAgent):
+    def __init__(self, alpha=0.25, gamma=0.75, delta=0.75, **kwargs):
+        super().__init__(**kwargs)
         self.gamma = float(gamma)
         self.alpha = float(alpha)
         self.delta = float(delta)
-        self.numTraining = int(numTraining)
         self.Q = dict()
         self.Pi = dict()
-        self.random = np.random.default_rng(int(kwargs["seed"])) if "seed" in kwargs else np.random.default_rng(12345)
-        self.rewards = Rewards(**kwargs)
-        self.previousState = None
-    
-    def registerInitialState(self, gameState):
-        self.rewards.initial(gameState)
-    
-    def final(self, state):
-        self.learn(self.previousState.state, state)
-        self.previousState = None
-        self.rewards.reset()
-        self.it += 1
 
-    def accessPiAndInitAll(self, state, numActions):
-        if state in self.Pi:
-            return self.Pi[state]
-        else:
-            Q = self.Q[state] = np.full(numActions, 0.)
-            Pi = self.Pi[state] = np.full(numActions, 1. / numActions)
-            return Pi
-    
+    def endGame(self, gameState):
+        self.learn(tuple(gameStateVector(gameState)), gameState)
+
+    def initAll(self, state, numActions):
+        if state not in self.Pi:
+            self.Q[state] = np.full(numActions, 0.0)
+            self.Pi[state] = np.full(numActions, 1.0 / numActions)
+
     def learn(self, state, gameState):
-        prevState = self.previousState.state
-        prevAction = self.previousState.action
-        prevNumActions = self.previousState.numActions
-        Q = self.Q[prevState]
-        Pi = self.Pi[prevState]
+        previousState = self.previousState.state
+        previousAction = self.previousState.action
+        numActions = len(self.previousState.validActions)
         reward = self.rewards(gameState)
         if state in self.Q:
             reward += self.gamma * np.amax(self.Q[state])
-        Q[prevAction] = (1 - self.alpha) * Q[prevAction] + self.alpha * reward
-        Pi[prevAction] += self.delta if  prevAction == np.argmax(Q) else -self.delta / (prevNumActions - 1)
-        Pi = np.maximum(Pi, np.full(prevNumActions, 0.))
-        self.Pi[prevState] = Pi / np.sum(Pi)
+        Q = self.Q[previousState]
+        Q[previousAction] = (1 - self.alpha) * Q[previousAction] + self.alpha * reward
+        Pi = self.Pi[previousState]
+        Pi[previousAction] += self.delta if previousAction == np.argmax(Q) else -self.delta / (numActions - 1)
+        Pi = np.maximum(Pi, np.full(numActions, 0.0))
+        self.Pi[previousState] = Pi / np.sum(Pi)
 
-    def getAction(self, gameState):
-        actions = gameState.getLegalActions()
-        serializedState = tuple(gameStateVector(gameState))
+    def selectAction(self, gameState, actions, actionsIndexes):
+        state = tuple(gameStateVector(gameState))
+        self.initAll(state, len(actions))
         if self.previousState != None:
-            self.learn(serializedState, gameState)
-        Pi = self.accessPiAndInitAll(serializedState, len(actions))
-        action = self.random.choice(actions, p=Pi)
-        self.previousState = QState(serializedState, actions.index(action), len(actions))
-        return action
- 
+            self.learn(state, gameState)
+        randomAction = self.random.choice([True, False], p=self.epsilonArray)
+        if randomAction:
+            action = self.random.integers(0, len(actions))
+        else:
+            Pi = self.Pi[state]
+            action = self.random.choice(list(range(len(actionsIndexes))), p=Pi)
+        self.previousState = PacmanState(state, action, 0, False, actionsIndexes)
+        return actions[action]
